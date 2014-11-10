9677a4c4625595e3f451acfdbbd19b29c2574c76
Aaron <aaron.storrer@gmail.com>
2014-11-10 11:44:42 -0500

    Removed Generated Tests

--- spec/models/area_spec.rb
+++ spec/models/area_spec.rb
@@ -1,5 +1,7 @@
 require 'spec_helper'
 
 describe Area do
-  pending "add some examples to (or delete) #{__FILE__}"
+  it "is valid with a name and number"
+  it "is invalid without a name"
+  it "is invalid without a number"
 end
--- spec/models/task_method_spec.rb
+++
@@ -1,5 +0,0 @@
-require 'spec_helper'
-
-describe TaskMethod do
-  pending "add some examples to (or delete) #{__FILE__}"
-end
