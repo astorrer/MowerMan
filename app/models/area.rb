10f47e4e28ecb660b911e9a4acfe3d52005d827d
Aaron <aaron.storrer@gmail.com>
2014-11-10 15:02:11 -0500

    Modified and Tested Areas

--- app/models/area.rb
+++ app/models/area.rb
@@ -1,4 +1,4 @@
 class Area < ActiveRecord::Base
-  validates :area, presence: true, length: { maximum: 45 }
-  validates :number, presence: true, numericality: { only_integer: true }, length: { maximum: 5 }
+  validates :name, presence: true, length: { maximum: 45 }
+  validates :number, presence: true, numericality: { only_integer: true }, length: { maximum: 5 }, uniqueness: true
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,13 +11,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20141106155651) do
+ActiveRecord::Schema.define(version: 20141110180215) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
   create_table "areas", force: true do |t|
-    t.string   "area"
+    t.string   "name"
     t.datetime "created_at"
     t.datetime "updated_at"
     t.integer  "number"
