48e04c29cb9215c1091c53d0565ada9324cbb7b3
Aaron <aaron.storrer@gmail.com>
2014-11-11 10:11:07 -0500

    Equipment Modification

--- spec/controllers/libraries_controller_spec.rb
+++ spec/controllers/libraries_controller_spec.rb
@@ -3,5 +3,10 @@ require 'spec_helper'
 describe LibrariesController do
   
   describe 'GET #index' do
+    it 'renders the :index template' do
+      get :library
+      expect(response).to render_template :index
+    end
   end
+
 end
