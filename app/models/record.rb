23f1286c5842831c6080644b410060ce2d614c05
Aaron <aaron.storrer@gmail.com>
2014-12-09 14:08:37 -0500

    Ensure Data is a Number

--- app/models/record.rb
+++ app/models/record.rb
@@ -4,6 +4,6 @@ class Record < ActiveRecord::Base
   # scope :thirty_day -> { where(created_at: (Time.now.midnight - 30.day)..Time.now.midnight) }
   
   validates :status, presence: true
-  validates :data, numericality: { only_integer: true }, :unless => '@upper_lower_bound.nil?'
+  validates :data, numericality: { only_integer: true }
   validates :comments, length: { maximum: 140 }
 end
