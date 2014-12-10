f15b20a8a632b17154c77634a8b8fc3f7a23fabd
Aaron <aaron.storrer@gmail.com>
2014-12-10 11:09:07 -0500

    Remove Integer Requirement for Records
    Converting Integers to Decimals

--- app/models/record.rb
+++ app/models/record.rb
@@ -4,6 +4,6 @@ class Record < ActiveRecord::Base
   # scope :thirty_day -> { where(created_at: (Time.now.midnight - 30.day)..Time.now.midnight) }
   
   validates :status, presence: true
-  validates :data, numericality: { only_integer: true }
+  validates :data, numericality: true
   validates :comments, length: { maximum: 140 }
 end
