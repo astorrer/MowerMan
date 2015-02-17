6204a59a1e19dffed585a096a74faf018517a50a
Aaron <aaron.storrer@gmail.com>
2015-02-17 15:20:53 +0000

    Database Changes

--- db/schema.rb
+++ db/schema.rb
@@ -11,28 +11,10 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150106135709) do
+ActiveRecord::Schema.define(version: 20150216202448) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
-  enable_extension "pg_stat_statements"
-
-  create_table "activities", force: true do |t|
-    t.integer  "trackable_id"
-    t.string   "trackable_type"
-    t.integer  "owner_id"
-    t.string   "owner_type"
-    t.string   "key"
-    t.text     "parameters"
-    t.integer  "recipient_id"
-    t.string   "recipient_type"
-    t.datetime "created_at"
-    t.datetime "updated_at"
-  end
-
-  add_index "activities", ["owner_id", "owner_type"], name: "index_activities_on_owner_id_and_owner_type", using: :btree
-  add_index "activities", ["recipient_id", "recipient_type"], name: "index_activities_on_recipient_id_and_recipient_type", using: :btree
-  add_index "activities", ["trackable_id", "trackable_type"], name: "index_activities_on_trackable_id_and_trackable_type", using: :btree
 
   create_table "areas", force: true do |t|
     t.string   "name"
@@ -237,6 +219,7 @@ ActiveRecord::Schema.define(version: 20150106135709) do
     t.integer  "overdue_count"
     t.integer  "department_id"
     t.integer  "egg_timer_id"
+    t.integer  "equipment_id"
   end
 
   create_table "procedures", force: true do |t|
